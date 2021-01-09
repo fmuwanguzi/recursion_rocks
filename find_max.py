479e2dab687cef347f8a734bce950b50fa0f886b
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-09 12:07:32 -0500

    [find_max] complete

--- find_max.py
+++ find_max.py
@@ -5,7 +5,11 @@
 
 def find_max(l):
     # Write code here
+    if len(l) == 1:
+        return l[0]
+    else:
+        return max(l[0], find_max(l[1:]))
     pass
 
-# print(find_max([1, 4, 45, 6, -50, 10, 2]))
+print(find_max([1, 4, 45, 6, -50, 10, 2]))
 # => 45
